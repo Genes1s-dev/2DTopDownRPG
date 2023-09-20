06377db1cb556242faf21cebcd6d7ecfdea1fd0d
Denis <volosetschii@mail.ru>
2023-09-20 13:18:11 +0300

    Arrows trap implementation continuation

--- Assets/Scripts/Environment/Turret.cs
+++ Assets/Scripts/Environment/Turret.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Turret : MonoBehaviour
 {
     private Animator animator;
-    [SerializeField] private GameObject arrow;
+    [SerializeField] private WeaponSO weaponSO;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform[] targetSpots;
+    [SerializeField] private Transform arrowStartPosition;
+    private Transform currentTarget;
+    private int index = 0;
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -13,12 +19,33 @@ public class Turret : MonoBehaviour
 
     private void Start()
     {
-        InvokeRepeating(nameof(Shoot), 3f, 3f);
+        InvokeRepeating(nameof(SetTarget), 3f, 3f);
     }
 
-    public void Shoot()
+    public void SetTarget()
     {
+        currentTarget = targetSpots[index];
+        if (index >= targetSpots.Length - 1)
+        {
+            index = 0;
+        } else {
+            index++;
+        }
+
         animator.SetTrigger("Shoot");
-        Instantiate(arrow, transform.position, Quaternion.identity);
+        StartCoroutine(nameof(Shoot));
+
+    }
+
+    private IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        GameObject arrow = Instantiate(arrowPrefab, arrowStartPosition.position, Quaternion.identity);
+
+        Vector3 shootingDir = currentTarget.position - arrowStartPosition.position;
+
+        arrow.transform.Rotate(0, 0, Mathf.Atan2(shootingDir.y, shootingDir.x) * Mathf.Rad2Deg);
+        arrow.GetComponent<Rigidbody2D>().velocity = shootingDir * weaponSO.projectileFlightSpeed;
     }
 }
